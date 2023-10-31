90c9658ace970a18f160ef9aba041019ded21b93
schorndorfer <wkt@northwestern.edu>
2023-10-31 12:04:01 -0500

    adding code example

---
+++ openai-helper/examples/extract-pdfs/main.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from tqdm import tqdm
+import logging
+
+
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+    print("hello world")
