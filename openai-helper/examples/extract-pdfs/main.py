18d75e06d02138fc65eda0b2a1b92c2c0936a3f8
William Thompson <will.k.t@gmail.com>
2023-11-07 21:36:37 -0600

    updates

--- openai-helper/examples/extract-pdfs/main.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from tqdm import tqdm
-import logging
-
-
-# -----------------------------------------------------------------------------
-if __name__ == "__main__":
-    print("hello world")
