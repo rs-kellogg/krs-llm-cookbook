9eea424430c63259b929996c77d14e65b29b3667
William Thompson <will.k.t@gmail.com>
2023-11-13 21:59:31 -0600

    updates

--- openai-helper/tests/test_cli.py
+++ openai-helper/tests/test_cli.py
@@ -1,19 +1,39 @@
 import os
 import tempfile
+from pathlib import Path
 from typer.testing import CliRunner
-from openaihelper.cli import *
+from openaihelper.cli import app
 import pytest
+
 
 runner = CliRunner()
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
-@pytest.mark.skip(reason="not working")
 def test_cli(tmpdir):
     result = runner.invoke(
         app,
-        ["count-tokens", "sample-100.csv", "config.yml", "--outdir", str(tmpdir)],
+        ["count-tokens", 
+         "--help"
+         ],
     )
     assert result.exit_code == 0
-    assert result.stdout.count("processing file:") == 100
+    assert "Usage: root count-tokens [OPTIONS] DATA_FILE_PATH CONFIG_FILE_PATH" in result.stdout
+    assert "Count tokens in a data file and write output to a csv file." in result.stdout
+
+
+    result = runner.invoke(
+        app,
+        ["count-tokens", 
+         str(dir_path/"data/arxiv_metadata-1.csv"), 
+         str(dir_path/"config.yml"), 
+         "--outdir", 
+         str(tmpdir),
+         ],
+    )
+    assert result.exit_code == 0
+    assert "arxiv_metadata-1_counts.csv" in os.listdir(tmpdir)
+    assert "id,count" in (tmpdir/"arxiv_metadata-1_counts.csv").read_text("utf-8")
+    assert "2310.00014,1346" in (tmpdir/"arxiv_metadata-1_counts.csv").read_text("utf-8")
+
